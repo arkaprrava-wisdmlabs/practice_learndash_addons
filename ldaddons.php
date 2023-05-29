048aebe430d882f031ce93fe19caacd3c6fdd9d5
“arkaprav” <chakraarkaprava@gmail.com>
2023-05-29 17:11:23 +0200

    created a plugin for learndash

---
+++ ldaddons.php
@@ -0,0 +1,7 @@
+<?php
+/*
+Plugin Name: Custom LearnDash Plugin
+Description: Adds a custom field to the group page in the group builder interface to set an group's course access date for group's users.
+Text Domain: ldad
+*/
+// Hook into the Group Builder page
